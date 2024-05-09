6fdb75d385d8bedda7e3497acfeb4d5e5635dc16
Naveen kumar <49843571+NKdesigndev@users.noreply.github.com>
2024-05-09 11:19:07 +0530

    Add files via upload

---
+++ question2.cpp
@@ -0,0 +1,63 @@
+
+#include <dos.h>
+#include <conio.h>
+#include <stdio.h>
+
+#define VGA_WIDTH 640
+#define VGA_HEIGHT 480
+#define VGA_MODE 0x13
+#define CIRCLE_COLOR 15 // Color for circles
+
+void setVideoMode(unsigned char mode) {
+    union REGS regs;
+    regs.h.ah = 0x00;
+    regs.h.al = mode;
+    int86(0x10, &regs, &regs);
+}
+
+void plotPixel(int x, int y, unsigned char color) {
+    unsigned char far *ptr = (unsigned char far *)0xA0000000;
+    ptr[y * VGA_WIDTH + x] = color;
+}
+
+void drawCircle(int centerX, int centerY, int radius) {
+    int x = radius, y = 0;
+    int radiusError = 1 - x;
+
+    while (x >= y) {
+        plotPixel(centerX + x, centerY + y, CIRCLE_COLOR);
+        plotPixel(centerX - x, centerY + y, CIRCLE_COLOR);
+        plotPixel(centerX + x, centerY - y, CIRCLE_COLOR);
+        plotPixel(centerX - x, centerY - y, CIRCLE_COLOR);
+        plotPixel(centerX + y, centerY + x, CIRCLE_COLOR);
+        plotPixel(centerX - y, centerY + x, CIRCLE_COLOR);
+        plotPixel(centerX + y, centerY - x, CIRCLE_COLOR);
+        plotPixel(centerX - y, centerY - x, CIRCLE_COLOR);
+
+        y++;
+
+        if (radiusError < 0) {
+            radiusError += 2 * y + 1;
+        } else {
+            x--;
+            radiusError += 2 * (y - x + 1);
+        }
+    }
+}
+
+int main() {
+    setVideoMode(VGA_MODE);
+
+    int centerX = VGA_WIDTH / 2; 
+    int centerY = VGA_HEIGHT / 2; 
+    int blobRadius = 10; 
+    int numCircles = 5; 
+
+    for (int i = 0; i < numCircles; ++i) {
+        drawCircle(centerX, centerY, blobRadius + i * 20);
+    }
+
+    getch(); 
+    setVideoMode(0x03); 
+    return 0;
+}
